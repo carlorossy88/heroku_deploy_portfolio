09d063d31188a310a3d9e03ea298ffd3e3b14996
Carlo Rossy <carlorossy88@gmail.com>
2020-12-01 17:22:59 -0500

    update package script w nodemon

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cdbfac777fb8978fe75d076e07a4d7e8400b128177529a86c11bfc51e7e301a
+oid sha256:f02f9c9d3a123d0c66f459cf72a1581b22f4fe9d37631569160d6722a7d6d42c
 size 296
