3b6701c1ff084a35e309203f9c7a9873ec5d0f5e
Carlo Rossy <carlorossy88@gmail.com>
2020-12-01 11:05:43 -0500

    first commit to initialize repo remotely

---
+++ app.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:50469f2c328c439809631a8a72b2fb6dc2078c699e20dd21281c14f23097ab2a
+size 296
---
+++ public/index.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:440eacb86482d404c85d4c124205b63c51ff619dfefdabcf4830fe216e07e1d4
+size 339
